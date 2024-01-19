74ec9250159e71fbdc0564444c550fbe9115910f
Steven Stone <dw395@byu.edu>
2024-01-19 14:26:42 -0700

    Added toString() override to make pretty boards

--- shared/src/main/java/chess/ChessBoard.java
+++ shared/src/main/java/chess/ChessBoard.java
@@ -1,5 +1,14 @@
 package chess;
 
+import java.util.Arrays;
+import java.util.Map;
+import java.lang.StringBuilder;
+import java.lang.Character;
+
+import static chess.ChessPiece.PieceType.*;
+import static chess.ChessGame.TeamColor.*;
+
+
 /**
  * A chessboard that can hold and rearrange chess pieces.
  * <p>
@@ -7,9 +16,8 @@ package chess;
  * signature of the existing methods.
  */
 public class ChessBoard {
-    private ChessPiece[][] board = new ChessPiece[8][8];
+    private final ChessPiece[][] board = new ChessPiece[8][8];
     public ChessBoard() {
-
     }
 
     /**
@@ -19,7 +27,7 @@ public class ChessBoard {
      * @param piece    the piece to add
      */
     public void addPiece(ChessPosition position, ChessPiece piece) {
-        board[position.getRow()][position.getColumn()] = piece;
+        board[position.getRow() - 1][position.getColumn() - 1] = piece;
     }
 
     /**
@@ -30,7 +38,7 @@ public class ChessBoard {
      * position
      */
     public ChessPiece getPiece(ChessPosition position) {
-        return board[position.getRow()][position.getColumn()];
+        return board[position.getRow() - 1][position.getColumn() - 1];
     }
 
     /**
@@ -38,6 +46,59 @@ public class ChessBoard {
      * (How the game of chess normally starts)
      */
     public void resetBoard() {
-        throw new RuntimeException("Not implemented");
+        addPiece(new ChessPosition(1, 1), new ChessPiece(WHITE, ROOK));
+        addPiece(new ChessPosition(1, 2), new ChessPiece(WHITE, KNIGHT));
+        addPiece(new ChessPosition(1, 3), new ChessPiece(WHITE, BISHOP));
+        addPiece(new ChessPosition(1, 4), new ChessPiece(WHITE, QUEEN));
+        addPiece(new ChessPosition(1, 5), new ChessPiece(WHITE, KING));
+        addPiece(new ChessPosition(1, 6), new ChessPiece(WHITE, BISHOP));
+        addPiece(new ChessPosition(1, 7), new ChessPiece(WHITE, KNIGHT));
+        addPiece(new ChessPosition(1, 8), new ChessPiece(WHITE, ROOK));
+        for (int i = 1; i < 8; i++) {
+            addPiece(new ChessPosition(2, i), new ChessPiece(WHITE, PAWN));
+        }
+        for (int i = 1; i < 8; i++) {
+            addPiece(new ChessPosition(7, i), new ChessPiece(BLACK, PAWN));
+        }
+        addPiece(new ChessPosition(8, 1), new ChessPiece(BLACK, ROOK));
+        addPiece(new ChessPosition(8, 2), new ChessPiece(BLACK, KNIGHT));
+        addPiece(new ChessPosition(8, 3), new ChessPiece(BLACK, BISHOP));
+        addPiece(new ChessPosition(8, 4), new ChessPiece(BLACK, KING));
+        addPiece(new ChessPosition(8, 5), new ChessPiece(BLACK, QUEEN));
+        addPiece(new ChessPosition(8, 6), new ChessPiece(BLACK, BISHOP));
+        addPiece(new ChessPosition(8, 7), new ChessPiece(BLACK, KNIGHT));
+        addPiece(new ChessPosition(8, 8), new ChessPiece(BLACK, ROOK));
     }
+    final static Map <ChessPiece.PieceType, Character> TypetocharMap = Map.of(
+            ChessPiece.PieceType.PAWN, 'p',
+            ChessPiece.PieceType.KNIGHT, 'n',
+            ChessPiece.PieceType.ROOK, 'r',
+            ChessPiece.PieceType.QUEEN, 'q',
+            ChessPiece.PieceType.KING, 'k',
+            ChessPiece.PieceType.BISHOP, 'b'
+    );
+
+    @Override
+    // override of string method to make nice box for debugging
+    public String toString() {
+        StringBuilder output = new StringBuilder();
+        for (int col = 8; col >= 1; col--){
+            output.append("|");
+            for (int row = 1; row <= 8; row++) {
+                var piece = getPiece(new ChessPosition(row, col));
+                if (piece == null){
+                    output.append(" |");
+                    continue;
+                }
+                Character type = TypetocharMap.get(piece.getPieceType());
+                type = piece.getTeamColor() == WHITE ? Character.toUpperCase(type) : type;
+                output.append(type);
+                output.append("|");
+            }
+            output.append("\n");
+        }
+
+        return output.toString();
+    }
+
 }
