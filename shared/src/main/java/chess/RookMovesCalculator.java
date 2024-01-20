fc5a993bc7957a867604dd44e575001ea22625f1
Steven Stone <dw395@byu.edu>
2024-01-19 23:47:59 -0700

    passes all tests

--- shared/src/main/java/chess/RookMovesCalculator.java
+++ shared/src/main/java/chess/RookMovesCalculator.java
@@ -5,6 +5,45 @@ import java.util.Collection;
 
 public class RookMovesCalculator {
     public static Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition myPosition) {
-        return new ArrayList<>();
+        ChessGame.TeamColor team = board.getPiece(myPosition).getTeamColor();
+        int row = myPosition.getRow();
+        int col = myPosition.getColumn();
+        int targetrow;
+        int targetcol;
+
+        ArrayList<ChessMove> moves = new ArrayList<>();
+
+        // I hate repeating code, so we're iterating through all four possible directions
+        for (int dir = 1; dir >= -1; dir -= 1) {                 // dir is forwards/backwards
+            for (int bias = 1; bias >= -1; bias -= 1) {          // bias is right/left
+                // forbidden skipping technique to minimize necessary code revisions don't @ me
+                if (dir * bias != 0){
+                    continue;
+                }
+                targetrow = row + dir;
+                targetcol = col + bias;
+
+                // while in the bounds of the board
+                while (targetrow >= 1 && targetrow <= 8 && targetcol >= 1 && targetcol <= 8) {
+
+                    // identify the target square
+                    ChessPiece target = board.getPiece(new ChessPosition(targetrow, targetcol));
+
+                    // if we hit an empty space, add it it to the available moves
+                    if (target == null) {
+                        moves.add(new chess.ChessMove(myPosition, new ChessPosition(targetrow, targetcol), null));
+                    } else {
+                        // if we hit a piece, check to see if its an enemy. If so, we can hit it, but no further
+                        if (target.getTeamColor() != team) {
+                            moves.add(new chess.ChessMove(myPosition, new ChessPosition(targetrow, targetcol), null));
+                        }
+                        break;
+                    }
+                    targetrow += dir;
+                    targetcol += bias;
+                }
+            }
+        }
+        return moves;
     }
 }
