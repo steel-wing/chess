ea1ff1970ce074b82770a03d2ed4a8d5a000a094
Steven Stone <dw395@byu.edu>
2024-02-25 22:00:44 -0700

    Implemented create()

--- server/src/main/java/server/CreateHandler.java
+++ server/src/main/java/server/CreateHandler.java
@@ -1,11 +1,41 @@
 package server;
 
+import dataAccess.DataAccessException;
+import request.CreateRequest;
+import service.CreateGameService;
 import spark.Request;
 import spark.Response;
 
 public class CreateHandler extends Handler {
     /** Create endpoint handler */
     public static Object create(Request req, Response res) {
-        return null;
+        // parse the incoming request info
+        String gameName = getBody(req, String.class);
+        String authToken = req.headers("authorization");
+        CreateRequest createRequest = new CreateRequest(gameName, authToken);
+
+        // check the input
+        if (gameName.isEmpty() || authToken.isEmpty()) {
+            return errorHandler("bad request", 400, res);
+        }
+
+        // initialize the output
+        int gameID;
+
+        try {
+            gameID = CreateGameService.create(createRequest);
+        } catch (DataAccessException exception) {
+            // handle the lack of authtoken exception
+            if (exception.getMessage().equals("No such AuthToken")) {
+                return errorHandler("unauthorized", 401, res);
+            }
+
+            // handle any other exceptions
+            return errorHandler(exception.getMessage(),500, res);
+        }
+
+        System.out.println("Game Created! ID:" + gameID);
+
+        return successHandler(gameID, res);
     }
 }
--- server/src/main/java/service/CreateGameService.java
+++ server/src/main/java/service/CreateGameService.java
@@ -1,4 +1,30 @@
 package service;
 
+import dataAccess.*;
+import model.AuthData;
+import model.GameData;
+import request.CreateRequest;
+
 public class CreateGameService {
+    /**
+     * @param gameRequest Contains the gameName and authToken
+     * @return the new gameID of the created game
+     * @throws DataAccessException in the event of authentication or retrieval errors
+     */
+    public static int create(CreateRequest gameRequest) throws DataAccessException {
+        // initialize our DAOs
+        AuthDAO authDao = new MemoryAuthDAO();
+        GameDAO gameDao = new MemoryGameDAO();
+
+        // get values from inputs
+        String gameName = gameRequest.gameName();
+        String authToken = gameRequest.authToken();
+
+        // get auth data (and throw errors)
+        AuthData auth = authDao.getAuth(gameRequest.authToken());
+
+        // create a new game and return it
+        GameData gameData = gameDao.createGame(gameName);
+        return gameData.gameID();
+    }
 }
