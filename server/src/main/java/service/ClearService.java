b1ab3adca1753a02caa1fdef4373043def7486ad
Steven Stone <dw395@byu.edu>
2024-02-28 19:02:35 -0700

    Added javadoc

--- server/src/main/java/service/ClearService.java
+++ server/src/main/java/service/ClearService.java
@@ -3,6 +3,10 @@ package service;
 import server.Server;
 
 public class ClearService {
+    /**
+     * Wipes all three databases.
+     * @return A success boolean.
+     */
     public static boolean clear() {
         return Server.authDAO.clear() && Server.gameDAO.clear() && Server.userDAO.clear();
     }
